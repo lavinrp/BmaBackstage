839db3036aac918e27feba9560fb2ef26a5c3e1f
Ryan Lavin <lavinrp@gmail.com>
2022-04-11 02:21:29 -0400

    i2  - Domain, Infrastructure, and UI project setup

---
+++ BmaBackstage.Domain/Class1.cs
@@ -0,0 +1,15 @@
+﻿namespace BmaBackstage.Domain
+{
+
+    public interface IStringGetter
+    {
+        string GetString();
+    }
+    public class Class1 : IStringGetter
+    {
+        public string GetString()
+        {
+            return "Domain";
+        }
+    }
+}
